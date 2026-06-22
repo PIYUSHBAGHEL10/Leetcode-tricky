d17b90db473bcf9ab40127a769a951890da99e19
PIYUSHBAGHEL10 <101945177+PIYUSHBAGHEL10@users.noreply.github.com>
2026-06-22 16:51:24 +0530

    Time: 78 ms (24.23%), Space: 32 MB (21.88%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.cpp
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     int nthUglyNumber(int n) {
         set<long> set;
-        long nthNumber = 1;
-        set.insert(nthNumber);
+        long no = 1;
+        set.insert(no);
         for(int i=0; i<n; i++){
-            nthNumber = *set.begin();
-            set.erase(nthNumber);
-            set.insert(nthNumber*2);
-            set.insert(nthNumber*3);
-            set.insert(nthNumber*5);
+            no = *set.begin();
+            set.erase(no);
+            set.insert(no*2);
+            set.insert(no*3);
+            set.insert(no*5);
         }
-        return nthNumber;
+        return no;
     }
 };
