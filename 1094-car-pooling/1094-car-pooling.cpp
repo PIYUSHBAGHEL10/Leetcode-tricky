ba69dad131c4d27ebb31b859cb5ed1ed11bf5bad
PIYUSHBAGHEL10 <101945177+PIYUSHBAGHEL10@users.noreply.github.com>
2026-06-24 18:04:40 +0530

    Time: 0 ms (100%), Space: 14.3 MB (68.99%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -1,41 +1,28 @@
 class Solution {
 public:
-    
-    static bool comp(vector<int> lhs, vector<int> rhs)
-    {
-        if(lhs[1]<rhs[1] || (lhs[1]==rhs[1] && lhs[2]<rhs[2]))
-            return true ;
-        return false ;
-    }
-        
     bool carPooling(vector<vector<int>>& trips, int capacity)
     {
-        int n = trips.size() ;
-		
-        sort(trips.begin(),trips.end(),comp) ;
+        vector<pair<int,int>> v ;
+        
+        for(int i=0;i<trips.size();i++)
+        {
+            v.push_back({trips[i][1],trips[i][0]}) ; // pushing fromi , numPassengersi
+            
+            v.push_back({trips[i][2],-trips[i][0]}) ; // pushing toi , -numPassengersi
+        }
+        
+        sort(v.begin(),v.end()) ;
         
         int curr = 0 ;
         
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq; // destinaton , numpassengers
-        
-        for(int i=0;i<n;i++)
+        for(int i=0;i<v.size();i++)
         {
-			// step - 1 : 
-            while(!pq.empty() && pq.top().first<=trips[i][1])
-            {
-                curr-=pq.top().second ;
-                pq.pop() ;
-            }
+            curr+=v[i].second ;
             
-			//step - 2
-            pq.push({trips[i][2],trips[i][0]}) ;
-            curr+=trips[i][0] ;
-			
-			//step - 3
             if(curr>capacity)
                 return false ;
         }
-		
-        return true ;  
+        
+        return true ;      
     }
 };
